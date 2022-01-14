a860eb2dab6a0259c18b8f8fffdb0c77e00d1565
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-13 22:50:14 -0600

    Class Cat

--- class.py
+++ class.py
@@ -7,9 +7,21 @@ Class Dog:
     def speak(self):
         return "Woof!"
     
-    def get_pet(pet="dog"):
+def get_pet(pet="dog"):
     
     '''the factory method'''
 
+    pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+    return pets[pet]
+
+Class Cat:
+'''Simple Cat class'''
+
+    def init_(self, name):
+        self._name = name
     
+    def speak(self):
+        return "Meow!"
     
+d = get_pet('dog')
+print(d.speak())
