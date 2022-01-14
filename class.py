b9c6593f3fec093cf888c9e3db8fcefa4901a5bf
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-13 22:39:52 -0600

    Class - Dog

---
+++ class.py
@@ -0,0 +1,15 @@
+Class Dog:
+'''Simple Dog class'''
+
+    def init_(self, name):
+        self._name = name
+    
+    def speak(self):
+        return "Woof!"
+    
+    def get_pet(pet="dog"):
+    
+    '''the factory method'''
+
+    
+    
