d96f39d74949dee9366c346ad61053bd4fdff3f1
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-14 21:31:48 -0600

    ...

--- class.py
+++ class.py
@@ -55,3 +55,23 @@ class PetStore:
 
     def __init__(self, pet_factory=None):
         '''pet_factory is our abstract factory'''
+        self._pet_factory = pet_factory
+
+    def show_pet(self):
+        '''utility method to display dtails of the objects returned'''
+        
+        pet= self._pet_factory.get_pet()
+        pet_food = self._pet_factory.get_food()
+
+        print("Our pet is '{}'!".format(pet))
+        print("Our pet says hello by '{}'".format(pet.speak()))
+        print("It's food is '{}'".format(pet_food))
+
+#create a concrete factory
+factory = DogFactory()
+
+#create a pet store housing our abstract factory
+shop = PetStore()
+
+#invoke the utility methos to show details of our pet
+shop.show_pet()
