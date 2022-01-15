2d92520e45571c022b28cc030a33a3f732e191f8
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-14 21:24:36 -0600

    ....

--- class.py
+++ class.py
@@ -28,3 +28,30 @@ print(d.speak())
 
 c = get_pet('cat')
 print(c.speak())
+
+class Dog:
+    '''one of the objects to be returned'''
+
+    def speak(self):
+        return "Woof!"
+
+    def __str__(self):
+        return "Dog"
+
+class DogFactory:
+    '''concrete factory'''
+    
+    def get_pet(self):
+        '''returns dog object'''
+        return Dog()
+
+    
+    def get_food(self):
+        '''returns dog food object'''
+        return "Dog Food!"
+
+class PetStore:
+    '''petstore houses our abstract factory'''
+
+    def __init__(self, pet_factory=None):
+        '''pet_factory is our abstract factory'''
