9b28e8c536be090660d751bef9f2f54c1b67da42
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-14 20:56:57 -0600

    corrected and run

--- class.py
+++ class.py
@@ -1,7 +1,7 @@
-Class Dog:
-'''Simple Dog class'''
+class Dog:
+    '''Simple Dog class'''
 
-    def init_(self, name):
+    def __init__(self, name):
         self._name = name
     
     def speak(self):
@@ -14,10 +14,10 @@ def get_pet(pet="dog"):
     pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
     return pets[pet]
 
-Class Cat:
-'''Simple Cat class'''
+class Cat:
+    '''Simple Cat class'''
 
-    def init_(self, name):
+    def __init__(self, name):
         self._name = name
     
     def speak(self):
@@ -25,3 +25,6 @@ Class Cat:
     
 d = get_pet('dog')
 print(d.speak())
+
+c = get_pet('cat')
+print(c.speak())
