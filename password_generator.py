38d83eea8e329b69b644e0a9335cf37ae075d6a7
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-18 22:54:40 -0600

    ...

--- password_generator.py
+++ password_generator.py
@@ -1,19 +1,35 @@
+import pandas as pd
 import random
 
 print('Password Generator')
 
-chars = 'abcdefghijklmnqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*().,?0123456789'
-number = input('Number of passwords: ')
+lower = 'abcdefghijklmnqrstuvwxyz'
+lower = str(lower)
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+upper = str(upper)
+symbol = '!@$%^&*().,?'
+symbol = str(symbol)
+numeric = '0123456789'
+numeric = str(numeric)
+
+number = input('Number of passwords needed?')
 number = int(number)
 
-length = input('Input your password length:')
+length = input('Password length?')
 length = int(length)
 
-print('\nhere are your passwords:')
+min_lower = ing(input('Min lower case letters?'))
+min_upper = int(input('Min upper case letters?'))
+min_symbol = int(input('Min symbols?'))
+min_num = int(input('Min Numbers?'))
+
+char_count = min_lower+min_upper+min_symbol+min_number
+
+print('\nHere are the passwords:')
 
 for pwd in range(number):
     passwords = ''
     for c in range(length):
-        passwords += random.choice(chars)    
+        passwords += random.choice(lower)+random.choice(upper)+random.choice(symbol)+random.choice(numeric)
     print(passwords)
 
