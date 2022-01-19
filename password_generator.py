29eaf404cfd1d565c79bb051df7d2a9e9a6c5e56
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-18 22:07:50 -0600

    finish

--- password_generator.py
+++ password_generator.py
@@ -10,3 +10,10 @@ length = input('Input your password length:')
 length = int(length)
 
 print('\nhere are your passwords:')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)    
+    print(passwords)
+
