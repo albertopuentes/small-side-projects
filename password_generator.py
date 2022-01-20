0e63c5c016f8695639a5f0c966d9f4ef23f065f7
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-19 22:28:35 -0600

    descrips added

--- password_generator.py
+++ password_generator.py
@@ -3,6 +3,7 @@ import random
 
 print('Password Generator')
 
+# pool of potential password characters
 lower = 'abcdefghijklmnqrstuvwxyz'
 lower = str(lower)
 upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -11,23 +12,28 @@ symbol = '!@$%^&*().,?'
 symbol = str(symbol)
 numeric = '0123456789'
 numeric = str(numeric)
+
+# containers combined into one
 options = lower+upper+symbol+numeric
 
+# Password 
 number = input('Number of passwords needed?')
 number = int(number)
-
 length = input('Password length?')
 length = int(length)
 
+# Minimun password requirements
 min_lower = int(input('Min lower case letters?'))
 min_upper = int(input('Min upper case letters?'))
 min_symbol = int(input('Min symbols?'))
 min_num = int(input('Min Numbers?'))
 
+# count of min required criteria
 char_count = min_lower+min_upper+min_symbol+min_num
 
 print('\nHere are the passwords:')
 
+# Generator using inputs and requirements
 for pwd in range(number):
     passwords = ''
     for c in range(min_lower):
