4f48a04997eb8b16e7259ed8264a395eea3ce0ae
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-21 12:15:56 -0600

    ...

--- simple_timer.py
+++ simple_timer.py
@@ -6,7 +6,7 @@ def countdown(t):
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
-        time.sleep(2)
+        time.sleep(1)
         t -= 1
     playsound('/Users/albertopuentes/Music/Music/Media.localized/Music/Unknown Artist/Unknown Album/Tolling_Bell.wav')
     print('Timer Completed')
