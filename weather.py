7676bf8748c6e892812b079b92487edff204811c
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-24 22:53:41 -0600

    deleted key

--- weather.py
+++ weather.py
@@ -1,9 +1,9 @@
-# API_key = 47ebd6c3604147b54e24bf6930ab064e
+# API_key = ''
 
 import requests
 from pprint import pprint
 
-API_Key = '47ebd6c3604147b54e24bf6930ab064e'
+API_Key = ''
 city = input("Enter a City: ")
 base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
 weather_data = requests.get(base_url).json()
