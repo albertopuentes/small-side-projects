a92af70118298e28d57af5f495930aef4d4a87b4
Alberto Puentes <albertopuentes00@gmail.com>
2022-01-25 12:46:01 -0600

    moved key to .gitignore file

--- weather.py
+++ weather.py
@@ -1,11 +1,11 @@
-# API_key = ''
+from pws import WeatherMap_IP 
 
 import requests
 from pprint import pprint
 
-API_Key = ''
+#API_Key = WeatherMap_IP
 city = input("Enter a City: ")
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+WeatherMap_IP+"&q="+city
 weather_data = requests.get(base_url).json()
 
 pprint(weather_data)
