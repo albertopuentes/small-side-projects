a1484610a44ed9724f89fb093b6792d4fd208589
Alberto Puentes <albertopuentes00@gmail.com>
2022-03-01 23:37:19 -0600

    coinbase api

---
+++ CryptoAlarm2.py
@@ -0,0 +1,23 @@
+
+import cbpro
+import pandas as pd
+
+from api import Coinbase_API
+from api import Coinbase_secret
+from coinbase.wallet.client import Client
+
+
+coinbase_API_key = 'Coinbase_API'
+coinbase_API_secret = 'Coinbase_secret'
+client = Client(coinbase_API_key, coinbase_API_secret)
+
+total = 0
+message = []
+
+accounts = client.get_accounts()
+for wallet in accounts.data:
+    message.append( str(wallet['name']) + ' ' +   str(wallet['native_balance']) )
+    value = str( wallet['native_balance']).replace('USD','')
+    total += float(value)
+message.append( 'Total Balance: ' + 'USD ' + str(total) )
+print ('\n'.join( message ))
