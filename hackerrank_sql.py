7169e7082cb51fa15d6e4ad31e9024f2418d610c
Alberto Puentes <albertopuentes00@gmail.com>
2022-05-07 09:26:34 -0500

    ...

--- hackerrank_sql.py
+++ hackerrank_sql.py
@@ -16,3 +16,15 @@ where c.company_code = l.company_code
     and m.manager_code = e.manager_code
 group by c.company_code
 order by c.company_code
+
+#2nd attempt
+select c.company_code, 
+        c.founder, 
+        count(DISTINCT l.lead_manager_code), 
+        count(DISTINCT s.senior_manager_code), 
+        count(DISTINCT m.manager_code), 
+        count(DISTINCT e.employee_code)
+from Company c
+    inner join Employees e on c.company_code = e.company_code
+group by c.company_code
+order by c.company_code
