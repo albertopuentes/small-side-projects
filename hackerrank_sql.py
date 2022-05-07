eda001cc8ee58a90070aed893bdbee03e3f6e018
Alberto Puentes <albertopuentes00@gmail.com>
2022-05-07 16:07:09 -0500

    ...

--- hackerrank_sql.py
+++ hackerrank_sql.py
@@ -50,3 +50,11 @@ where LAT_N < 137.2345 and LAT_N > 38.7880;
 select round(max(LAT_N), 4)
 from STATION
 where LAT_N < 137.2345;
+
+#Query the Western Longitude (LONG_W) for the largest 
+# Northern Latitude (LAT_N) in STATION that is less than . 
+# Round your answer to  decimal places.
+
+select round(LONG_W, 4)
+from STATION
+where LAT_N = (select max(LAT_N) from STATION where LAT_N < 137.2345);
