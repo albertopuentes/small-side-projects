1042b280fd3cb315853a494b5c5ff79e15c0959a
Alberto Puentes <albertopuentes00@gmail.com>
2022-05-08 18:05:17 -0500

    ...

--- hackerrank_sql.py
+++ hackerrank_sql.py
@@ -107,3 +107,15 @@ select round(LAT_N, 4)
 from (select ROW_NUMBER() OVER(ORDER BY LAT_N) AS RowNum, LAT_N
       from STATION) as t
 where t.RowNum = 250
+
+#Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. 
+# Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report 
+# must be in descending order by grade -- i.e. higher grades are entered first. If there is more
+#  than one student with the same grade (8-10) assigned to them, order those particular students 
+# by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and 
+# list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+# Write a query to help Eve.
+
+select if (Grade < 8, null, Name), Grade, Marks
+from Students s Join Grades g on Marks between Min_Mark and Max_Mark
+order by g.Grade desc, s.Name, s.Marks
