47b96559bb071e9b6c8d2f048a57341c39267bad
Alberto Puentes <albertopuentes00@gmail.com>
2022-06-06 15:40:53 -0500

    Scrapy Learn project

---
+++ ietf_scraper/ietf_scraper/spiders/IETF.py
@@ -0,0 +1,13 @@
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = 'IETF'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
+
+    def parse(self, response):
+        #css example: title = response.css('span.title::text').get()
+        #xpath example:
+        title = response.xpath('//span[@class="phone"]/text()').get()
+        return {"title": title}
